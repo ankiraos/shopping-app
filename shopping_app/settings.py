87a9dc6ce4601eb48ec46d216535bdedd5c1fa01
Ankita Joahi <joshiankita5825%@gmail.com>
2024-09-04 17:53:01 +0530

    add static root in settings

--- shopping_app/settings.py
+++ shopping_app/settings.py
@@ -1,6 +1,6 @@
 # Application definition
 from .secret import *
-
+import os
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -81,6 +81,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
